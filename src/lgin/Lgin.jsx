b20fb23eba58298a48d2ae5362fd3c248c74e68b
Minhazul Abdin <hey_minhaz@Minhazuls-MacBook-Air.local>
2023-10-10 04:22:55 +0600

    register

--- src/lgin/Lgin.jsx
+++ src/lgin/Lgin.jsx
@@ -5,65 +5,67 @@
 import { Form, Link, NavLink } from "react-router-dom";
 
 const Lgin = () => {
-  const handelLgin = (e) => {
+ 
+
+  const handelLogin = e => {
+    
     e.preventDefault();
-    console.log(e.currentTarget);
-    // eslint-disable-next-line no-undef
-    const email = form.get('email');
-    const password = form.get('password');
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
     console.log(email,password);
-  };
+
+  }
+  
 
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body w-[400px] ">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="password"
-                  className="input input-bordered"
-                  required
-                />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
-                <label className="label">
-                  <p>
-                    {" "}
-                    Don't have account?{" "}
-                    <Link
-                      className="label-text-alt btn"
-                      to="/Register"
-                    >
-                      Register
-                    </Link>
-                  </p>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
-            </form>
-          </div>
+        <h1 className=" text-4xl mb-8">Hey, Login your account</h1>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handelLogin} className="card-body lg:w-[400px] ">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+              <label className="label">
+                <p>
+                  {" "}
+                  Don't have account?{" "}
+                  <Link className="label-text-alt btn" to="/Register">
+                    Register
+                  </Link>
+                </p>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/register/Register.jsx
+++ src/register/Register.jsx
@@ -1,149 +1,82 @@
 /* eslint-disable react/no-unescaped-entities */
 
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
+
+   const handelRegister = (e) => {
+     e.preventDefault();
+
+     const email = e.target.email.value;
+     const password = e.target.password.value;
+     const name = e.target.name.value;
+     console.log(name,email, password);
+   };
+
+
     return (
-      <div data-aos="fade-up">
-        <section className="gradient-form h-full bg-neutral-200 ">
-          <div className="container h-full p-10 lg:ml-20">
-            <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
-              <div className="w-full">
-                <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
-                  <div className="g-0 lg:flex lg:flex-wrap">
-                    <div className="px-4 md:px-0 lg:w-6/12">
-                      <div className="md:mx-6 md:p-12">
-                        <div className="text-center">
-                          <img
-                            className="mx-auto w-48"
-                            src="https://images.squarespace-cdn.com/content/v1/55ce79cee4b0ef57d7cb12b8/1606884575371-O0IJD68CNXY3HAV9O2FY/PrimaryLogo.png?format=2500w"
-                            alt="logo"
-                          />
-                          <h4 className="mb-12 mt-1 pb-1 text-xl font-semibold">
-                            Creating unforgettable experiences.
-                          </h4>
-                        </div>
-
-                        <form>
-                          <p className="mb-4">Please login to your account</p>
-
-                          <div
-                            className="relative mb-4"
-                            data-te-input-wrapper-init
-                          >
-                            <input
-                              type="name"
-                              required
-                              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                              id="exampleFormControlInput1"
-                              placeholder="Name"
-                            />
-                            <label
-                              htmlFor="exampleFormControlInput1"
-                              className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                            >
-                              Name
-                            </label>
-                          </div>
-
-                          <div
-                            className="relative mb-4"
-                            data-te-input-wrapper-init
-                          >
-                            <input
-                              type="email"
-                              required
-                              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                              id="exampleFormControlInput1"
-                              placeholder="Email"
-                            />
-                            <label
-                              htmlFor="exampleFormControlInput1"
-                              className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                            >
-                              Email
-                            </label>
-                          </div>
-
-                          <div
-                            className="relative mb-4"
-                            data-te-input-wrapper-init
-                          >
-                            <input
-                              type="password"
-                              required
-                              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                              id="exampleFormControlInput11"
-                              placeholder="Password"
-                            />
-                            <label
-                              htmlFor="exampleFormControlInput11"
-                              className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                            >
-                              Password
-                            </label>
-                          </div>
-
-                          <div className="mb-12 pb-1 pt-1 text-center">
-                            <button
-                              className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
-                              type="button"
-                              data-te-ripple-init
-                              data-te-ripple-color="light"
-                              style={{
-                                background:
-                                  "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
-                              }}
-                            >
-                              Register
-                            </button>
-                          </div>
-
-                          <div className="flex items-center justify-between pb-6">
-                            <p className="mb-0 mr-2">Don't have an account?</p>
-                            <NavLink to="/Lgin">
-                              <p
-                                className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-                                data-te-ripple-init
-                                data-te-ripple-color="light"
-                              >
-                                Login
-                              </p>
-                            </NavLink>
-                          </div>
-                        </form>
-                      </div>
-                    </div>
-
-                    <div
-                      className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
-                      style={{
-                        background:
-                          "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
-                      }}
-                    >
-                      <div className="px-4 py-6 text-white md:mx-6 md:p-12">
-                        <h4 className="mb-6 text-xl font-semibold">
-                          We are more than just a company
-                        </h4>
-                        <p className="text-sm">
-                          An event management company provides professional
-                          services to plan, organize, and execute various types
-                          of events for individuals, organizations, or
-                          businesses. These events can range from small-scale
-                          gatherings to large-scale conferences and everything
-                          in between. Here's a breakdown of the typical services
-                          offered by an event management company
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+      <div>
+        <div className="hero min-h-screen bg-base-200">
+          <h1 className=" text-4xl mb-8">Hey, Login your account</h1>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handelRegister} className="card-body lg:w-[400px] ">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="name"
+                  name="name"
+                  placeholder="Name"
+                  className="input input-bordered"
+                  required
+                />
               </div>
-            </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+                <label className="label">
+                  <p>
+                    {" "}
+                    Already have account?{" "}
+                    <Link className="label-text-alt btn" to="/Lgin">
+                      Login
+                    </Link>
+                  </p>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Register</button>
+              </div>
+            </form>
           </div>
-        </section>
+        </div>
       </div>
     );
 };
